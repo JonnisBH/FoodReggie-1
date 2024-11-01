d63d3e32d0e5a6216eb75e38b9a0e2fa20523a80
AndreasHatt <hattmann2003@gmail.com>
2024-11-01 19:42:47 +0100

    Lagde en ny grid view for foods. La til navigering på table og grid. Lagde flere matvarer i FoodController og skilte GetFoods fra IAction.

--- Controllers/FoodController.cs
+++ Controllers/FoodController.cs
@@ -6,20 +6,67 @@ namespace FoodReggie_1.Controllers;
 
 public class FoodController : Controller{
     public IActionResult Table(){
+        var foods = GetFoods();
+        ViewBag.CurrentViewName = "Table";
+        return View(foods);
+    }
+
+    public IActionResult Grid(){
+        var foods = GetFoods();
+        ViewBag.CurrentViewName = "Grid";
+        return View(foods);
+    }
+
+    public List<Food> GetFoods(){
         var foods = new List<Food>();
         var food1 = new Food{
             FoodId = 1,
             Name = "Kebab",
-            Price = 69
+            Price = 69,
+            Description = "Delicious kebab with homemade pita bread, fresh vegetables, meat and our own special kebab dressing.",
+            ImageURL = "/images/kebab.jpg"
         };
         var food2 = new Food{
             FoodId = 2,
             Name = "Hamburger",
-            Price = 89
+            Price = 89,
+            Description = "Hamburger with bacon, cheddar cheese, lettuce, tomatoes, onions and our own special hamburger dressing.",
+            ImageURL = "/images/hamburger.jpg"
+        };
+        var food3 = new Food{
+            FoodId = 3,
+            Name = "Fried chicken",
+            Price = 79,
+            Description = "Crispy fried chicken made with quality meat from local Norwegian farmers.",
+            ImageURL = "/images/friedChicken.jpg"
+        };
+        var food4 = new Food{
+            FoodId = 4,
+            Name = "Tacos",
+            Price = 59,
+            Description = "Freshly made tacos with lettuce, tomatoes, corn, beef and sour cream. Optional: Guacamole.",
+            ImageURL = "/images/tacos.jpg"
+        };
+        var food5 = new Food{
+            FoodId = 5,
+            Name = "Pancakes",
+            Price = 99,
+            Description = "Fluffy pancakes served with syrup and butter on top.",
+            ImageURL = "/images/pancakes.jpg"
+        };
+        var food6 = new Food{
+            FoodId = 6,
+            Name = "Pizza",
+            Price = 109,
+            Description = "American pizza with tomato sauce, mozzarella and optional toppings",
+            ImageURL = "/images/pizza.jpg"
         };
         foods.Add(food1);
         foods.Add(food2);
-        ViewBag.CurrentViewName = "Food menu";
-        return View(foods);
+        foods.Add(food3);
+        foods.Add(food4);
+        foods.Add(food5);
+        foods.Add(food6);
+        return foods;
     }
 }
--- Models/Food.cs
+++ Models/Food.cs
@@ -6,6 +6,6 @@ namespace FoodReggie_1.Models
         public string Name { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public string? Description { get; set; }
-        public string? Image { get; set; }
+        public string? ImageURL { get; set; }
     }
 }
