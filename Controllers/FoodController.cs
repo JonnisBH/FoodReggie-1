4031b1a7d39f4bc77bf37e06f5e3fd3f1d62b9c1
AndreasHatt <hattmann2003@gmail.com>
2024-11-04 16:42:22 +0100

    La til CRUD funksjonalitet i applikasjonen

--- Controllers/FoodController.cs
+++ Controllers/FoodController.cs
@@ -2,6 +2,7 @@ using System;
 using FoodReggie_1.Models;
 using FoodReggie_1.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage.Json;
 
 namespace FoodReggie_1.Controllers;
 
@@ -21,5 +22,59 @@ public class FoodController : Controller{
         List<Food> foods = _foodDbContext.Foods.ToList();
         var foodViewModel = new FoodViewModel(foods, "Grid");
         return View(foodViewModel);
+    }
+
+    [HttpGet]
+    public IActionResult Create(){
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Food food){
+        if(ModelState.IsValid){
+            _foodDbContext.Foods.Add(food);
+            _foodDbContext.SaveChanges();
+            return RedirectToAction(nameof(Table));
+        }
+        return View(food);
+    }
+
+    [HttpGet]
+    public IActionResult Update(int id){
+        var food = _foodDbContext.Foods.Find(id);
+        if (food == null){
+            return NotFound();
+        }
+        return View(food);
+    }
+
+    [HttpPost]
+    public IActionResult Update(Food food){
+        if (ModelState.IsValid){
+            _foodDbContext.Foods.Update(food);
+            _foodDbContext.SaveChanges();
+            return RedirectToAction(nameof(Table));
+        }
+        return View(food);
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int id){
+        var food = _foodDbContext.Foods.Find(id);
+        if(food == null){
+            return NotFound();
+        }
+        return View(food);
+    }
+
+    [HttpPost]
+    public IActionResult ConfirmDelete(int id){
+        var food = _foodDbContext.Foods.Find(id);
+        if(food == null){
+            return NotFound();
+        }
+        _foodDbContext.Foods.Remove(food);
+        _foodDbContext.SaveChanges();
+        return RedirectToAction(nameof(Table));
     }
 }
