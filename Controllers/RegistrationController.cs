0d10ddb4bb88cd2a153617838e08acb73d470eec
AndreasHatt <hattmann2003@gmail.com>
2024-11-06 16:16:27 +0100

    Lagde Views for User og Registrations. Lagde controller for User og Registration.

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using FoodReggie_1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodReggie_1.Controllers;
+
+public class RegistrationController : Controller{
+    private readonly FoodDbContext _foodDbContext;
+
+    public RegistrationController(FoodDbContext foodDbContext){
+        _foodDbContext = foodDbContext;
+    }
+
+    public async Task<IActionResult> Table(){
+        List<Registration> registrations = await _foodDbContext.Registrations.ToListAsync();
+        return View(registrations);
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using FoodReggie_1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodReggie_1.Controllers;
+
+public class UserController : Controller{
+    private readonly FoodDbContext _foodDbContext;
+
+    public UserController(FoodDbContext foodDbContext){
+        _foodDbContext = foodDbContext;
+    }
+
+    public async Task<IActionResult> Table(){
+        List<User> users = await _foodDbContext.Users.ToListAsync();
+        return View(users);
+    }
+}
